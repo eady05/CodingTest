727174d2760f61aa15d464c462c7212ffbdefc0c
eady05 <87294455+eady05@users.noreply.github.com>
2025-12-20 17:46:41 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = "";
+    int os = overwrite_string.length() + s;
+    int ms = my_string.length();
+    answer = my_string.substr(0,s) + overwrite_string + my_string.substr(os,ms);
+    return answer;
+}

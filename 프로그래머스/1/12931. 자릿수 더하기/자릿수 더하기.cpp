f8358d70523694fe86bd20f58438327530e374ff
eady05 <87294455+eady05@users.noreply.github.com>
2026-01-08 20:56:28 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    while(n){
+        answer += n%10;
+        n = n/10;
+    }
+    return answer;
+}

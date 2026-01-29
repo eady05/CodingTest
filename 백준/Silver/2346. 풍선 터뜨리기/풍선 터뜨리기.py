cbc131e4fd213aa4d8d37333d87ce021c1d7aab4
eady05 <87294455+eady05@users.noreply.github.com>
2026-01-29 16:56:36 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 56 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -9,16 +9,14 @@ d = deque()
 for i in range(n):
     d.append((i + 1, n_list[i]))
 
-while d:
-    idx, k = d.popleft()
-    print(idx, end=' ')
-    
-    # 풍선이 다 터졌으면 종료
-    if not d:
-        break
-    
-    # 3. 핵심: k가 양수면 왼쪽으로, 음수면 오른쪽으로 회전!
-    if k > 0:
-        d.rotate(-(k - 1))
-    else:
-        d.rotate(-k)
+while True:
+  idx, val = d.popleft()
+  print(idx, end=' ')
+
+  if(len(d)==0):
+    break;
+
+  if(val>0):
+    d.rotate(-(val-1))
+  else:
+    d.rotate(-val)

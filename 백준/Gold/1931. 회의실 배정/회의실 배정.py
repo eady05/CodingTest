f7dd91e6826b137e62bc2f244b156494b7dedfe9
eady05 <87294455+eady05@users.noreply.github.com>
2026-01-31 16:27:42 +0900

    [Gold V] Title: 회의실 배정, Time: 168 ms, Memory: 47556 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,23 +1,20 @@
-import sys
-
+import sys 
+from collections import deque
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-n=int(input())
-time=[]
-
+n = int(input())
+time = []
 for _ in range(n):
-  s,e=map(int, input().split())
-  time.append((s,e))
+  s,e = map(int, input().split())
+  time.append((e,s))
 
-time.sort(key=lambda x : (x[1], x[0]))
-
-cnt=0
-end_t=0
-
-for s,e in time:
-  if(end_t<=s):
+time.sort()
+prev = 0
+cnt = 0
+for t in time:
+  if(prev<=t[1]):
+    prev = t[0]
     cnt+=1
-    end_t=e
-
 
 print(cnt)

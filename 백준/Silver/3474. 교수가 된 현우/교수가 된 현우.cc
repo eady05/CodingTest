7e83e8082e796758f71675351ae403d600efdb52
eady05 <87294455+eady05@users.noreply.github.com>
2026-03-16 23:38:40 +0900

    [Silver III] Title: 교수가 된 현우, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n, a;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        cin>>a;
+        int ret2 = 0, ret5 =0;
+        for(int j=2; j<=a; j*=2)
+        {
+            ret2+=a/j;
+        }
+        for(int j=5; j<=a; j*=5)
+        {
+            ret5+=a/j;
+        }
+        cout << min(ret2,ret5) <<"\n";
+    }
+    
+    return 0;
+}

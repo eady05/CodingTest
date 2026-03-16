05e70c1be4a1fe186d69480d136275970a331344
eady05 <87294455+eady05@users.noreply.github.com>
2026-03-16 23:20:44 +0900

    [Silver V] Title: 기상캐스터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.cc
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.cc
@@ -1,29 +1,30 @@
-// C++ 코드를 작성하세요
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int h,w;
-
-
-int main() { 
-  cin>>h>>w;
-  for(int i=0; i<h; i++){
-    string city;
-    int cpos = -1;
-    cin>>city;
-    for(int j=0; j<city.size(); j++){
-      if(city[j]=='c'){
-        cout<<0<<' ';
-        cpos = j;
-      }
-      else if(cpos!=-1){
-        int t = j-cpos;
-        cout<<t<<' ';
-      }
-      else cout<<cpos<<' ';
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int h,w,t;
+    char c;
+    cin>>h>>w;
+    for(int i=0; i<h; i++)
+    {
+        t=-1;
+        for(int j=0; j<w; j++)
+        {
+            cin>>c;
+            if(c == 'c')
+            {
+                t=0;
+            }
+            else if(t!=-1)
+            {
+                t+=1;
+            }
+            cout<<t<<' ';
+        }
+        cout<<'\n';
     }
-    cout<<'\n';
-  }
-  
-  return 0; 
+    
+    return 0;
 }

ccc98fe065c8d9d6c706c28ddd1ca1a77340d388
eady05 <87294455+eady05@users.noreply.github.com>
2026-03-24 11:35:56 +0900

    [Silver V] Title: 영화감독 숌, Time: 64 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin>>n;
+    int i = 666;
+    for(;;i++)
+    {
+        if(to_string(i).find("666") != string::npos)n--;
+        if(n==0)break;
+    }
+    cout<<i<<'\n';
+}
